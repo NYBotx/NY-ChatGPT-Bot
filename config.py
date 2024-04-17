995ef0b5ab9efb9107cffbd5abf9497c6fadaad1
NYBotx <167092531+NYBotx@users.noreply.github.com>
2024-04-17 12:09:54 +0530

    Update config.py

--- config.py
+++ config.py
@@ -10,12 +10,12 @@ def is_enabled(value, default):
     else:
         return default
 
-API_ID = int(environ.get("API_ID", ""))
-API_HASH = environ.get("API_HASH", "")
-BOT_TOKEN = environ.get("BOT_TOKEN", "")
-LOG_CHANNEL = int(environ.get("LOG_CHANNEL", ""))
+API_ID = int(environ.get("API_ID", "19911978"))
+API_HASH = environ.get("API_HASH", "e3f5848d4c384af9e6f1f52ca84c19c7")
+BOT_TOKEN = environ.get("BOT_TOKEN", "7097497120:AAHgCulZw3AYU2pTFZ5L3rsGK5VNXM62inI")
+LOG_CHANNEL = int(environ.get("LOG_CHANNEL", "-1002081616844"))
 ADMINS = int(environ.get("ADMINS", ""))
-DB_URI = environ.get("DB_URI", "")
-DB_NAME = environ.get("DB_NAME", "chatgptvjbot")
-OPENAI_API = environ.get("OPENAI_API", "")
+DB_URI = environ.get("DB_URI", "mongodb+srv://nischay419:nischay419@cluster0.z6hynou.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+DB_NAME = environ.get("DB_NAME", "chatgptnybot")
+OPENAI_API = environ.get("OPENAI_API", "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")
 AI = is_enabled((environ.get("AI","True")), False)
